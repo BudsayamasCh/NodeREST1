fcb36f76534076bde0d6aeeb72451b2a235fa850
BudsayamasCh <budsayamasnithi21@gmail.com>
2024-01-31 10:25:28 +0700

    up index port 5000

--- index.js
+++ index.js
@@ -83,5 +83,5 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port http://localhost:${port}...`));
